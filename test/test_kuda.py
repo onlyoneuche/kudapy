5e217a7763edb91b04da479bf11172685bda1da1
daleentontech <daleentontech@gmail.com>
2020-08-04 22:21:15 +0100

    added test for creating virtual accounts

--- test/test_kuda.py
+++ test/test_kuda.py
@@ -15,3 +15,19 @@ def test_user_can_fetch_bank_list():
     #response = response.json()
 
     assert response["Message"] == "Completed Successfully"
+
+def test_user_can_create_virtual_account():
+    private_key = load_private_key()
+    public_key = load_public_key()
+    client_key = "7QuX12xfmSpFl8d3a54b"
+    request_ref = math.floor(random.random() * 1000000000000 + 1)
+    tracking_reference = f"vAcc{generate_id(5)}"
+    response = kuda(public_key, private_key, client_key)("CREATE_VIRTUAL_ACCOUNT", request_ref, {
+        "email": "daon@gmail.com",
+        "phoneNumber": "09034514310",
+        "firstName": "kelechechukwu",
+        "lastName": "Imman",
+        "trackingReference": tracking_reference
+        })
+
+    assert response["Status"] == True
