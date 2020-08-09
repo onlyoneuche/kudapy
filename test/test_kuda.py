10d24738e5236e04886adb56d42bbdb2b236effb
daleentontech <daleentontech@gmail.com>
2020-08-09 11:22:25 +0100

    564783

--- test/test_kuda.py
+++ test/test_kuda.py
@@ -29,3 +29,18 @@ def test_user_can_create_virtual_account():
         })
 
     assert response["Status"] == False
+
+
+def test_user_can_make_name_enquiry():
+    private_key = load_private_key()
+    public_key = load_public_key()
+    client_key = "7QuX12xfmSpFl8d3a54b"
+    request_ref = get_request_reference()
+    #tracking_reference = get_request_reference()
+    response = kuda(public_key, private_key, client_key)("NAME_ENQUIRY", request_ref, {
+        "beneficiaryAccountNumber": "2070527575",
+        "bankCode": "033",
+        # "senderTrackingReference": None
+        })
+
+    assert response["Status"] == True
