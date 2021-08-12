e43ca7d43eec38e8568b7e7f365ef49a6e3d6851
iameo <okwudike.emmanuel@gmail.com>
2021-08-12 05:12:19 +0100

    test for retrieve_main_account_balance - passes

--- tests/test_kuda.py
+++ tests/test_kuda.py
@@ -54,6 +54,10 @@ def test_retrieve_virtual_account_balance(kuda_instance):
     status, resp = kuda_instance.retrieve_virtual_account_balance()
     assert status
 
+def test_retrieve_main_account_balance(kuda_instance):
+    status, resp = kuda_instance.retrieve_main_account_balance()
+    assert status
+
 
 """
 def test_create_account_with_nuban():
